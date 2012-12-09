aaf4e2a6dc5ee1de83159f7823747e4832123381
Tasso Tirapani Silva Pinto <tasso.t@gmail.com>
2012-12-08 22:07:34 -0200

    Fix DelimiterSearchStrategy.java

--- src/org/grview/syntax/analyzer/gsll1/error/DelimiterSearchStrategy.java
+++ src/org/grview/syntax/analyzer/gsll1/error/DelimiterSearchStrategy.java
@@ -1,19 +1,18 @@
 package org.grview.syntax.analyzer.gsll1.error;
 
+import java.util.Stack;
+
 import org.grview.output.AppOutput;
 import org.grview.output.HtmlViewer.TOPIC;
 import org.grview.syntax.analyzer.gsll1.AnalyzerAlternative;
 import org.grview.syntax.analyzer.gsll1.AnalyzerStackRepository;
 import org.grview.syntax.analyzer.gsll1.AnalyzerTableRepository;
 import org.grview.syntax.analyzer.gsll1.AnalyzerToken;
-import org.grview.syntax.model.GrViewStack;
-import org.grview.syntax.model.NTerminalStack;
 import org.grview.syntax.model.TableNode;
 
 public class DelimiterSearchStrategy extends IErroStrategy
 {
 	private AnalyzerTableRepository analyzerTable;
-	private AnalyzerStackRepository analyzerStack;
 	private AnalyzerToken analyzerToken;
 
 	public DelimiterSearchStrategy()
@@ -28,12 +27,11 @@ public class DelimiterSearchStrategy extends IErroStrategy
 	public int tryFix(int UI, int column, int line)
 	{
 		int IX;
-
 		IX = UI;
 
 		int I = -1;
 
-		NTerminalStack pilhaNaoTerminalY = new NTerminalStack();
+		Stack<Integer> pilhaNaoTerminalY = new Stack<Integer>();
 
 		init();
 
@@ -41,26 +39,19 @@ public class DelimiterSearchStrategy extends IErroStrategy
 		{
 			if (analyzerTable.getGraphNode(IX).IsTerminal())
 			{
-
 				TableNode terminalNode = analyzerTable.getTermial(analyzerTable.getGraphNode(IX).getNodeReference());
 
-				String temp = analyzerTable.getTermial(analyzerTable.getGraphNode(IX).getNodeReference()).getName();
-				if (temp.equals(analyzerToken.getCurrentSymbol()))
+				if (terminalNode.getName().equals(analyzerToken.getCurrentSymbol()))
 				{
 					AppOutput.displayText("<font color='red'>Symbol \"" + terminalNode.getName() + "\" at before column " + column + " assumed as delimiter.\n</font>", TOPIC.Output);
 					I = IX;
 				}
 				else
 				{
-
 					if (analyzerTable.getGraphNode(IX).getSucessorIndex() != 0)
 					{
 						IX = analyzerTable.getGraphNode(IX).getSucessorIndex();
 
-					}
-					else if (analyzerTable.getGraphNode(IX).isLambda())
-					{
-						IX = analyzerTable.getGraphNode(IX).getSucessorIndex();
 					}
 					else
 					{
@@ -70,19 +61,25 @@ public class DelimiterSearchStrategy extends IErroStrategy
 			}
 			else
 			{
-				pilhaNaoTerminalY.push(IX);
-				if(analyzerTable.getGraphNode(IX).getSucessorIndex() != 0)
+				if (!pilhaNaoTerminalY.empty() && pilhaNaoTerminalY.contains(IX))
 				{
 					IX = analyzerTable.getGraphNode(IX).getSucessorIndex();
+					if (IX != 0 && !pilhaNaoTerminalY.empty())
+					{
+						analyzerTable.getGraphNode(pilhaNaoTerminalY.pop()).getAlternativeIndex();
+					}
+					else
+					{
+						IX = 0;
+					}
 				}
-				
-				if(IX != 0)
+				else
 				{
-					analyzerTable.getGraphNode(pilhaNaoTerminalY.pop()).getAlternativeIndex();
+					pilhaNaoTerminalY.push(IX);
+					IX = analyzerTable.getNTerminal(analyzerTable.getGraphNode(IX).getNodeReference()).getFirstNode();
 				}
 			}
 		}
-
 		if (I < 0)
 		{
 			restore(false);
